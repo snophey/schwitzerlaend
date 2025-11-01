44b9b3f23071ad95746ca95114c5bde28f554052
JuliTheProgrammer <juli.leipert@gmail.com>
2025-11-01 13:54:09 +0100

    make TrainingsPlanItem, calculate type and exercises

--- code/frontend/app/routes/home.tsx
+++ code/frontend/app/routes/home.tsx
@@ -1,7 +1,8 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { TrainingsPlanItem } from "~/components/TrainingsPlanItem";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +10,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex justify-center align-center">
+      <TrainingsPlanItem
+        title="Krafttraining"
+        day_number={1}
+        day="Monday"
+        exercises={[{"name":"Push-ups","type":"repetition","reps":15,"description":"Perform 15 push-ups with proper form"},{"name":"Pull-ups","type":"repetition","reps":10,"description":"Complete 10 pull-ups or assisted pull-ups"},{"name":"Plank","type":"time","duration_sec":60,"description":"Hold plank position for 60 seconds"},{"name":"Bench Press","type":"weighted repetition","reps":8,"weight":75.0,"description":"Perform 8 reps of bench press with 75kg load"}]}
+      />
+    </div>
+  );
 }
